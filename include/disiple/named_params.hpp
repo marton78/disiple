7366ab65790afe31a398af45f98f8e43c6d57b2f
Márton Danóczy <marton78@gmail.com>
2023-01-11 08:58:40 +0100

    Enable named type parameters

--- include/disiple/named_params.hpp
+++ include/disiple/named_params.hpp
@@ -10,87 +10,109 @@ namespace disiple {
     struct List;
 
     template <typename T, template <T> class Tag>
-    struct Required;
+    struct RequiredValue;
 
     template <typename T, template <T> class Tag, T Default>
-    struct Optional;
+    struct OptionalValue;
+
+    template <template <typename> class Tag>
+    struct RequiredType;
+
+    template <template <typename> class Tag, typename Default>
+    struct OptionalType;
 
     
     // Errors
 
     template <typename T, template <T> class Tag>
-    struct ParameterNotFound;
+    struct RequiredValueNotFound;
+
+    template <template <typename> class Tag>
+    struct RequiredTypeNotFound;
 
     template <typename... Args>
     struct UnusedArguments;
 
 
-    // Implementation of ExtractRequired and ExtractOptional
+    // Implementation of Extract
 
-    // Helper templates to extract parameters from arguments. The two lists are:
+    // Helper template to extract parameters from arguments. The two lists are:
     //    * ListArgs: all elements yet to be processed
     //    * ListDone: all elements except for the one that has been found (by default empty)
-    template <typename ListArgs, typename T, template <T> class Tag, typename ListDone = List<>>
-    struct ExtractRequired;
+    template <typename ListArgs, typename Parameter, typename ListDone = List<>>
+    struct Extract;
     
-    template <typename ListArgs, typename T, template <T> class Tag, T Default, typename ListDone = List<>>
-    struct ExtractOptional;
-
     // If the current element in ListArgs is the Tag we're looking for, we're done
     template <typename T, template <T> class Tag, T Value, typename... Tail, typename... Done>
-    struct ExtractRequired<List<Tag<Value>, Tail...>, T, Tag, List<Done...>>
+    struct Extract<List<Tag<Value>, Tail...>, RequiredValue<T, Tag>, List<Done...>>
     {
         using This = Tag<Value>;
         using Rest = List<Done..., Tail...>;
     };
 
     template <typename T, template <T> class Tag, T Value, T Default, typename... Tail, typename... Done>
-    struct ExtractOptional<List<Tag<Value>, Tail...>, T, Tag, Default, List<Done...>>
+    struct Extract<List<Tag<Value>, Tail...>, OptionalValue<T, Tag, Default>, List<Done...>>
     {
         using This = Tag<Value>;
         using Rest = List<Done..., Tail...>;
     };
 
+    template <template <typename> class Tag, typename Type, typename... Tail, typename... Done>
+    struct Extract<List<Tag<Type>, Tail...>, RequiredType<Tag>, List<Done...>>
+    {
+        using This = Tag<Type>;
+        using Rest = List<Done..., Tail...>;
+    };
+
+    template <template <typename> class Tag, typename Type, typename Default, typename... Tail, typename... Done>
+    struct Extract<List<Tag<Type>, Tail...>, OptionalType<Tag, Default>, List<Done...>>
+    {
+        using This = Tag<Type>;
+        using Rest = List<Done..., Tail...>;
+    };
+
     // Otherwise, the current element in ListArgs is not what we're looking for.
     // Remove it from ListArgs, append it to ListDone, and continue with the next element
-    template <typename Head, typename... Tail, typename T, template <T> class Tag, typename... Done>
-    struct ExtractRequired<List<Head, Tail...>, T, Tag, List<Done...>>
-    : ExtractRequired<List<Tail...>, T, Tag, List<Done..., Head>>
-    {};
-
-    template <typename Head, typename... Tail, typename T, template <T> class Tag, T Default, typename... Done>
-    struct ExtractOptional<List<Head, Tail...>, T, Tag, Default, List<Done...>>
-    : ExtractOptional<List<Tail...>, T, Tag, Default, List<Done..., Head>>
+    template <typename Head, typename... Tail, typename Parameter, typename... Done>
+    struct Extract<List<Head, Tail...>, Parameter, List<Done...>>
+    : Extract<List<Tail...>, Parameter, List<Done..., Head>>
     {};
 
     // If we have processed all elements in ListArgs, we're done
     template <typename T, template <T> class Tag, typename... Done>
-    struct ExtractRequired<List<>, T, Tag, List<Done...>>
-    : ParameterNotFound<T, Tag>
+    struct Extract<List<>, RequiredValue<T, Tag>, List<Done...>>
+    : RequiredValueNotFound<T, Tag>
     {};
 
     template <typename T, template <T> class Tag, T Default, typename... Done>
-    struct ExtractOptional<List<>, T, Tag, Default, List<Done...>>
+    struct Extract<List<>, OptionalValue<T, Tag, Default>, List<Done...>>
+    {
+        using This = Tag<Default>;
+        using Rest = List<Done...>;
+    };
+
+    template <template <typename> class Tag, typename... Done>
+    struct Extract<List<>, RequiredType<Tag>, List<Done...>>
+    : RequiredTypeNotFound<Tag>
+    {};
+
+    template <template <typename> class Tag, typename Default, typename... Done>
+    struct Extract<List<>, OptionalType<Tag, Default>, List<Done...>>
     {
         using This = Tag<Default>;
         using Rest = List<Done...>;
     };
 
 
+
     // Implementation of Parameters
 
-    // If the current parameter is Optional, extract it and continue processing
-    template <typename... Args, typename T, template <T> class Tag, T Default, typename... Tail>
-    struct Parameters<List<Args...>, Optional<T, Tag, Default>, Tail...>
-    : ExtractOptional<List<Args...>, T, Tag, Default>::This
-    , Parameters<typename ExtractOptional<List<Args...>, T, Tag, Default>::Rest, Tail...>
-    {};
 
-    // If the current parameter is Required, extract it and continue processing
-    template <typename... Args, typename T, template <T> class Tag, typename... Tail>
-    struct Parameters<List<Args...>, Required<T, Tag>, Tail...>
-    : ExtractRequired<List<Args...>, T, Tag>::This
-    , Parameters<typename ExtractRequired<List<Args...>, T, Tag>::Rest, Tail...>
+    // Extract the current parameter and continue processing
+    template <typename... Args, typename Parameter, typename... Tail>
+    struct Parameters<List<Args...>, Parameter, Tail...>
+    : Extract<List<Args...>, Parameter>::This
+    , Parameters<typename Extract<List<Args...>, Parameter>::Rest, Tail...>
     {};
 
     // If there are still Args left, but all Params have been processed, throw an error
--- test/named_params.cpp
+++ test/named_params.cpp
@@ -5,28 +5,38 @@ using namespace disiple;
 
 template <int N> struct Foo { static constexpr int foo = N; };
 template <int N> struct Bar { static constexpr int bar = N; };
+template <typename T> struct Baz { using baz = T; };
+template <typename T> struct Qux { using qux = T; };
 
 template <typename... Args>
 struct Example
 {
     using P = Parameters<
         List<Args...>,
-        Required<int, Foo>,
-        Optional<int, Bar, -1>
+        RequiredType<Baz>,
+        OptionalType<Qux, float>,
+        RequiredValue<int, Foo>,
+        OptionalValue<int, Bar, -1>
     >;
 };
 
 TEST_CASE("Named Parameters", "[named_params]")
 {
-    using Ex1 = Example<Foo<5>>;
+    using Ex1 = Example<Foo<5>, Baz<double>>;
     STATIC_REQUIRE( Ex1::P::foo == 5  );
     STATIC_REQUIRE( Ex1::P::bar == -1 );
+    STATIC_REQUIRE( std::is_same<Ex1::P::baz, double>::value );
+    STATIC_REQUIRE( std::is_same<Ex1::P::qux, float>::value );
 
-    using Ex2 = Example<Foo<6>, Bar<7>>;
+    using Ex2 = Example<Foo<6>, Bar<7>, Baz<int>, Qux<void>>;
     STATIC_REQUIRE( Ex2::P::foo == 6 );
     STATIC_REQUIRE( Ex2::P::bar == 7 );
+    STATIC_REQUIRE( std::is_same<Ex2::P::baz, int>::value );
+    STATIC_REQUIRE( std::is_same<Ex2::P::qux, void>::value );
 
-    using Ex3 = Example<Bar<7>, Foo<6>>;
+    using Ex3 = Example<Bar<7>, Foo<6>, Qux<void>, Baz<int>>;
     STATIC_REQUIRE( Ex3::P::foo == 6 );
     STATIC_REQUIRE( Ex3::P::bar == 7 );
+    STATIC_REQUIRE( std::is_same<Ex3::P::baz, int>::value );
+    STATIC_REQUIRE( std::is_same<Ex3::P::qux, void>::value );
 }
